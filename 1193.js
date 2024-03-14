dd27310e9f2a30b98251503b506c78261fa88a74
EunSo0 <dmstn423@naver.com>
2024-03-14 09:32:36 +0900

    일반수학1

--- 1193.js
+++ 1193.js
@@ -1,23 +1,17 @@
 const input = require("fs").readFileSync("input.txt").toString().trim();
-
-let n = +input;
-let i = 1;
+const num = +input;
+let n = num;
+let i = 0;
 
 while (true) {
   n -= i;
-  if ((n === 1) | (n <= 0)) break;
+  if (n <= 0) break;
   i++;
 }
 
-console.log((i * (i + 1)) / 2 - n);
-
-// 1 / 1
-// 1 / 2
-// 2 / 1
-// 3 / 1
-// 2 / 2
-// 1 / 3
-// 1 / 4
-// 2 / 3
-// 3 / 2
-// 4 / 1
+const a = (i * (i + 1)) / 2 - num;
+if (i % 2 === 0) {
+  console.log(`${i - a}/${a + 1}`);
+} else {
+  console.log(`${a + 1}/${i - a}`);
+}
