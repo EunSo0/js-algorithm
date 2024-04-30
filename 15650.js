390c22b05ba5a734a68dd7e57b4da03a7321984e
EunSo0 <dmstn423@naver.com>
2024-04-30 16:05:40 +0900

    백트래킹 알고리즘

--- 15650.js
+++ 15650.js
@@ -4,25 +4,21 @@ const [n, m] = input.split(" ").map(Number);
 const arr = Array.from({ length: n }, (_, i) => i + 1);
 const visited = new Array(n).fill(false);
 const selected = [];
-let answer = "";
 
-function dfs(arr, depth) {
+function dfs(depth, start) {
   if (depth === m) {
-    let result = [];
-    for (let i of selected) result.push(i);
-    for (let x of result) answer += x + " ";
-    answer += "\n";
+    let result = "";
+    for (let i of selected) result += i + " ";
+    console.log(result);
+    return;
   }
-  for (let i = 0; i < n; i += 1) {
+  for (let i = start; i < arr.length; i += 1) {
     if (visited[i]) continue;
     selected.push(arr[i]);
     visited[i] = true;
-    dfs(arr, depth + 1);
+    dfs(depth + 1, i + 1);
     selected.pop();
     visited[i] = false;
-    
   }
 }
-
-dfs(arr, 0);
-console.log(answer);
+dfs(0, 0);
