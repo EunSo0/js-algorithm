466058140e8b4f8a2213dd9de180ad4306efa4ab
EunSo0 <dmstn423@naver.com>
2024-05-05 15:05:36 +0900

    백트래킹 알고리즘

--- 2529.js
+++ 2529.js
@@ -3,3 +3,42 @@ const input = require("fs")
   .toString()
   .trim()
   .split("\n");
+
+const n = +input[0];
+const arr = input[1].split(" ");
+const visited = Array(10).fill(false);
+const select = [];
+let current = "";
+let first = "";
+
+function dfs(depth) {
+  if (depth === n + 1) {
+    let check = true;
+    for (let i = 0; i < n; i += 1) {
+      if (arr[i] === "<") {
+        if (select[i] > select[i + 1]) check = false;
+      } else if (arr[i] === ">") {
+        if (select[i] < select[i + 1]) check = false;
+      }
+    }
+    if (check) {
+      current = "";
+      for (let x of select) current += x + "";
+      if (first === "") first = current;
+    }
+    return;
+  }
+  for (let i = 0; i < 10; i += 1) {
+    if (visited[i]) continue;
+    select.push(i);
+    visited[i] = true;
+    dfs(depth + 1);
+    select.pop();
+    visited[i] = false;
+  }
+}
+
+dfs(0);
+
+console.log(current);
+console.log(first);
