d74b7074cc4761d96c7822715a5ca9797c14b58d
EunSo0 <dmstn423@naver.com>
2024-06-23 17:39:32 +0900

    dp

--- dp/1904.js
+++ dp/1904.js
@@ -2,4 +2,15 @@ const input = require('fs')
   .readFileSync('input.txt')
   .toString()
   .trim()
-  .split('\n');
+  .split(' ');
+
+const N = +input[0];
+const arr = Array(1000000).fill(0);
+arr[1] = 1;
+arr[2] = 2;
+
+for (let i = 3; i <= N; i += 1) {
+  arr[i] = (arr[i - 1] + arr[i - 2]) % 15746;
+}
+
+console.log(arr[N]);
