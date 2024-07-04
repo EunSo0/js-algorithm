d2428bf7f9100e84d52972bdf6b5b994a057a5d9
EunSo0 <dmstn423@naver.com>
2024-07-04 19:02:21 +0900

    dp

--- dp/10844.js
+++ dp/10844.js
@@ -3,3 +3,21 @@ const input = require('fs')
   .toString()
   .trim()
   .split('\n');
+
+const n = +input[0];
+
+const dp = Array.from({ length: n + 1 }, () => new Array(10).fill(0));
+for (let i = 1; i <= 9; i += 1) {
+  dp[1][i] = 1;
+}
+
+for (let i = 2; i <= n; i += 1) {
+  for (j = 0; j <= 9; j += 1) {
+    if (j == 0) dp[i][j] = dp[i - 1][j + 1];
+    else if (j === 9) dp[i][j] = dp[i - 1][j - 1];
+    else dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
+    dp[i][j] %= Number(1e9);
+  }
+}
+
+console.log(dp[n].reduce((a, b) => a + b) % Number(1e9));
